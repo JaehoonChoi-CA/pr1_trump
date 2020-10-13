0cf556dfe95b07faeaaa0c29e47eceeab6a06706
Jaehoon Choi <denny719@naver.com>
2020-10-12 23:43:40 -0700

    finally connected

--- server/router/fred.js
+++ server/router/fred.js
@@ -1,28 +1,56 @@
 const express = require('express');
 const firebase = require('../config/firebase');
+const spawn = require('child_process').spawn;
+
 const axios = require('axios');
 const {
   getData,
 } = require('../utils/fred');
+console.log(11);
+const pythonOptions= {
+  pythonPath: '/Library/Frameworks/Python.framework/Versions/3.6/bin/python3'
+}
 
 const router = express.Router();
 
+// /api/fred/all
 router.get('/all', async (req, res) => {
-  try {
-    const snapshot = await firebase.db.collection('primary').get()
-    const contents = snapshot.docs.map(doc => ({ ...doc.data() }));
-    console.log(contents[0])
-    res.send({
-      message: 'success',
-      contents,
+  firebase.db.collection('primary').get()
+    .then(snapshot => {
+      const contents = snapshot.docs.map(doc => {
+        return {
+          ...doc.data(),
+        }
+      });
+      // console.log(11, contents);
+      const process = spawn('python3', [
+        'python/main.py',
+        JSON.stringify(contents),
+      ]);
+      let result = '';
+      process.stdout.on('data', (data) => {
+        result += data.toString('utf8');
+        // return res.send({
+        //   message: 'success',
+        //   result,
+        // })
+      });
+      process.on('close', () => {
+        console.log(33, typeof JSON.parse(result));
+        result = JSON.parse(result);
+        return res.send({
+          message: 'success',
+          result,
+        });
+      });
     })
-  } catch (err) {
-    console.error(err);
-    res.send({
-      message: 'fail',
-      contents: [],
+    .catch((err) => {
+      console.error(err);
+      res.send({
+        message: 'fail',
+        contents: [],
+      });
     });
-  }
 });
 
 router.post('/update', async (req, res) => {
